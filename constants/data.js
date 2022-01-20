5ddde52d458e08d3cffb260ba27bb402404b42d5
Bryan Silva <bryan@leadboxhq.com>
2022-01-20 18:26:48 -0300

    first commit

---
+++ constants/data.js
@@ -0,0 +1,98 @@
+const userData = {
+  githubUsername: "bryan2811",
+  name: "Bryan Silva",
+  designation: "Front-End Developer",
+  actualCompanyLink: "https://leadboxhq.com/",
+  avatarUrl: "/avatar.jpg",
+  email: "brandonb2816@gmail.com",
+  phone: "+54 342-522-0522",
+  address: "Santa Fe, Argentina",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I started learning programming when I was 16 years old 👨🏻‍💻 and I fell in love with the Front-End stack, in 2019 I got a Platzi Scholarship that helped me to keep learning while I was still in Venezuela. In 2020 I migrated to Argentina and had the opportunity to participate in a project that would go to the University of Buenos Aires in 2021, in which I contributed my Front-End skills to update some views of this system.`,
+      `Also in that year I started working with Leadbox, a Canadian company that develops solutions to improve the conversions of different car dealers, such as Ford, Honda, Nissan, Toyota, among others.`,
+      `The technologies I work with on a daily basis are: Tailwind, PHP, VueJS, Javascript, Wordpress.`,
+      `I also have knowledge in: React, NodeJS, Web Performance, SEO, Google Analytics, Google Tag Manager, Data Studio.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Front-End Developer",
+      company: "Leadbox HQ",
+      year: "2021",
+      companyLink: "https://leadboxhq.com/",
+      desc: "I am currently working in the Core Team developing new features, I am also in charge of improving the Core Web Vitals of each of the sites so that they have a good performance. I use SEO tools such as Ahrefs to check the status of the sites and make the corresponding corrections. I also take care of the development of new sites and the creation of Google Analytics accounts and customized Data Studio reports.",
+    },
+    {
+      title: "Front-End Developer",
+      company: "Fundación EPyCA",
+      year: "2020",
+      companyLink: "http://info.reprasis.org/",
+      desc: "I was in charge of converting some old Flash views into HTML and CSS and fixing some bugs in the Reprasis system, a platform of the foundation, so that it could work properly before the start of the course at the University of Buenos Aires (UBA).",
+    },
+    {
+      title: "Student",
+      company: "Platzi",
+      year: "2021 ♾",
+      companyLink: "https://platzi.com/",
+      desc: "I started my first steps in programming on this platform and I am still using it today to continue learning.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/bryansilva28",
+    twitter: "https://twitter.com/devsilv",
+    linkedin: "https://www.linkedin.com/in/bryansilva28/",
+    github: "https://github.com/bryan2811",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
