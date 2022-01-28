d733ff94fcc01ed20c8c5f4761182853a2f66d2c
Bryan Silva <brandonb2816@gmail.com>
2022-01-28 13:28:50 -0300

    Adding GTM integration

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Script from 'next/script'
 import { useRouter } from "next/router";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
@@ -42,6 +43,18 @@ export default function ContainerBlock({ children, ...customMeta }) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer', 'GTM-N3SHVTX');
+        `,
+        }}
+      />
       <main className="w-full dark:bg-gray-800">
         <Navbar />
         <div>{children}</div>
